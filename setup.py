d842606f707ddfeb1f0298398c673251ed23d894
Louis Taylor <kragniz@gmail.com>
2014-05-30 13:46:31 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -1,15 +1,12 @@
 from distutils.core import setup
 
-with open('README.md') as file:
-        long_description = file.read()
-
 setup(
     name='goatd_client',
-    version='0.1',
+    version='0.1.2',
     author='Louis Taylor',
     author_email='kragniz@gmail.com',
     description=('Python wrapper for the goatd API, used to write behavior scripts.'),
-    long_description=long_description,
+    long_description=open("README.rst").read(),
     license='GPL',
     keywords='goat sailing wrapper rest',
     url='https://github.com/goatd/python-goatd',
