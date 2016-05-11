540da2d60f03689fe543277f3874357e7b3b5cde
Louis Taylor <louis@kragniz.eu>
2016-05-11 03:30:36 +0100

    Modernize time

--- setup.py
+++ setup.py
@@ -4,20 +4,16 @@ except ImportError:
     from distutils.core import setup
 
 setup(
-    name='goatd_client',
+    name='python-goatdclient',
     version='0.1.4',
     author='Louis Taylor',
-    author_email='kragniz@gmail.com',
+    author_email='louis@kragniz.eu',
     description=('Python wrapper for the goatd API, used to write behavior scripts.'),
     long_description=open("README.rst").read(),
     license='GPL',
     keywords='goat sailing wrapper rest',
     url='https://github.com/goatd/python-goatd',
-    packages=['goatd_client'],
-    requires=['docopt'],
-    install_requires=[
-        'docopt >= 0.6'
-        ],
+    packages=['goatdclient'],
     classifiers=[
         'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
