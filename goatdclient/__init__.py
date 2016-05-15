07a9c6ec485d9d916a2c056df8c195a658141263
Louis Taylor <louis@kragniz.eu>
2016-05-15 14:17:19 +0100

    Add behaviour client support

--- goatdclient/__init__.py
+++ goatdclient/__init__.py
@@ -1,1 +1,2 @@
 from .goatd_client import Goat
+from .goatd_client import Behaviour
--- goatdclient/goatd_client.py
+++ goatdclient/goatd_client.py
@@ -11,8 +11,8 @@ import json
 
 Wind = namedtuple('Wind', ['direction', 'speed'])
 
-class Goat(object):
-    '''A goat controlled by goatd'''
+
+class Goatd(object):
     def __init__(self, host='localhost', port=2222):
         '''
         Create a goat instance, connecting to goatd at `host` on port `port`
@@ -20,69 +20,107 @@ class Goat(object):
         self.host = host
         self.port = port
 
-    def goatd_url(self, endpoint):
+    def url(self, endpoint):
         '''Return a formatted url pointing at `endpoint` on the goatd server'''
         return 'http://{0}:{1}{2}'.format(self.host, self.port, endpoint)
 
-    def _get(self, endpoint):
+    def get(self, endpoint):
         '''Return the result of a GET request to `endpoint` on goatd'''
-        json_body = urlopen(self.goatd_url(endpoint)).read().decode('utf-8')
+        json_body = urlopen(self.url(endpoint)).read().decode('utf-8')
         return json.loads(json_body)
 
-    def _post(self, content, endpoint=''):
+    def post(self, content, endpoint=''):
         '''
         Issue a POST request with `content` as the body to `endpoint` and
         return the result.
         '''
-        url = self.goatd_url(endpoint)
+        url = self.url(endpoint)
         post_content = json.dumps(content).encode('utf-8')
         headers = {'Content-Type': 'application/json'}
         request = Request(url, post_content, headers)
-        return urlopen(request)
+
+        response = urlopen(request)
+
+        return json.loads(response.read().decode('utf-8'))
 
     def quit(self):
-        content = self._post({'quit': True}, '/')
+        content = self.post({'quit': True}, '/')
         print(content)
+
+
+class Goat(object):
+    '''A goat controlled by goatd'''
+
+    def __init__(self, goatd=None):
+        if goatd is None:
+            self.goatd = Goatd()
+        else:
+            self.goatd = goatd
 
     @property
     def heading(self):
         '''Return the current heading of the goat in degrees'''
-        content = self._get('/goat')
+        content = self.goatd.get('/goat')
         return float(content.get('heading'))
 
     @property
     def wind(self):
         '''Return the direction of the wind in degrees'''
-        content = self._get('/wind')
+        content = self.goatd.get('/wind')
         return Wind(content.get('direction'), content.get('speed'))
 
     @property
     def position(self):
         '''Return a tuple in the form `(latitude, longitude)`'''
-        content = self._get('/goat')
+        content = self.goatd.get('/goat')
         return tuple(content.get('position'))
 
     @property
     def version(self):
         '''Return the version of goatd'''
-        content = self._get('/')
+        content = self.goatd.get('/')
         return content.get('goatd').get('version')
 
     def rudder(self, angle):
         '''Set the angle of the rudder to be `angle` degrees'''
-        request = self._post({'value': angle}, '/rudder')
+        request = self.goatd.post({'value': angle}, '/rudder')
         content = json.loads(request.read().decode('utf-8'))
         return content.get('result')
 
     def sail(self, angle):
         '''Set the angle of the sail to `angle` degrees'''
-        request = self._post({'value': angle}, '/sail')
+        request = self.goatd.post({'value': angle}, '/sail')
         content = json.loads(request.read().decode('utf-8'))
         return content.get('result')
 
+
+class Behaviour(object):
+    def __init__(self, goatd=None):
+        if goatd is None:
+            self.goatd = Goatd()
+        else:
+            self.goatd = goatd
+
+    def _get_behaviour_data(self):
+        return self.goatd.get('/behaviours')
+
+    def list(self):
+        return list(self._get_behaviour_data().get('behaviours').keys())
+
+    def start(self, name):
+        d = self.goatd.post({'current': name}, endpoint='/behaviours')
+        current = d.get('current')
+        if current is not None:
+            return 'started {}'.format(current)
+        else:
+            return 'no behaviour running'
+
+    def stop(self):
+        self.start(None)
+
+
 if __name__ == '__main__':
     goat = Goat()
-    print(goat._get(''))
     print(goat.version)
     print(goat.heading)
     print(goat.wind)
