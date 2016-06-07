d9deb16242c42c7e45ef77e55a429a815b73ac76
Louis Taylor <louis@kragniz.eu>
2016-06-07 04:35:10 +0100

    Add get_current_waypoints to __init__

--- goatdclient/__init__.py
+++ goatdclient/__init__.py
@@ -1,6 +1,6 @@
 from .goatd_client import Goat  # noqa
 from .goatd_client import Behaviour  # noqa
-from .goatd_client import Waypoints  # noqa
+from .goatd_client import get_current_waypoints  # noqa
 
 from .point import Point  # noqa
 from .bearing import Bearing  # noqa
